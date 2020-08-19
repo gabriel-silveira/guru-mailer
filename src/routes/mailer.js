b3a75a83d551b15642b145614618652abbec307d
Gabriel Silveira <gabriel@gabrielsilveira.com.br>
2020-08-19 13:26:23 -0300

    Done

--- src/routes/mailer.js
+++ src/routes/mailer.js
@@ -4,7 +4,7 @@ const nodemailer  = require('nodemailer');
 
 routes.post('/', async (req, res) => {
   try {
-    const { to, subject, html } = req.body;
+    const { from, to, subject, html } = req.body;
 
     const options = {
       host: process.env.MAILER_SMTP,
@@ -13,20 +13,20 @@ routes.post('/', async (req, res) => {
       auth: {
         user: process.env.MAILER_USER,
         pass: process.env.MAILER_PASSWORD
-      },
-      sendmail: true
+      }
     }
-  
+
     const transporter = nodemailer.createTransport(options);
-  
+
     const info = await transporter.sendMail({
       from: process.env.MAILER_USER, // sender address
+      replyTo: from,
       to: to ? to : 'gabrielsilveira.web@gmail.com', // list of receivers
       subject, // Subject line
       // text: 'Hello world?', // plain text body
       html // html body
     });
-    
+
     res.status(200).json(info);
   } catch (error) {
     res.status(500).json(error);
